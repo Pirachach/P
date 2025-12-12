5c7c7a1943d11e0720324edc88ade6190c759fc5
Pirachach <pirachach_T@cmu.ac.th>
2025-12-13 00:02:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int inte = -1;
+    int even = 0;
+    int odd = 0;
+    while (inte != 0){
+        cout << "Enter an integer: ";
+        cin >> inte;
+        if (inte>0 and inte%2 == 0){
+            even++;
+        }
+        if (inte%2 != 0){
+            odd++;
+        }
+        }
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
